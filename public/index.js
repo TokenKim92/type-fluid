5bcba0b8e394fb14b10c4528556a180d1316b6b4
Token Kim <TokenKim92@gmail.com>
2022-11-01 20:10:14 +0100

    refactor: public files

--- public/index.js
+++ public/index.js
@@ -1,7 +1,4 @@
 import TypeFluid from '../src/typeFluid.js';
 
-const type = new TypeFluid('type', 50000, {
-  waveHeight: 1,
-  rippleSpeed: 5,
-});
+const type = new TypeFluid('type', 15);
 type.start();
