aa8ac3420cd8fcd447f6dfec0d942868cb4876b3
Token Kim <tokenkim92@gmail.com>
2023-01-01 19:38:58 +0100

    refactor: update page

---
+++ index.js
@@ -0,0 +1,4 @@
+import TypeFluid from './src/TypeFluid.js';
+
+const type = new TypeFluid('type', 15);
+type.start();
--- public/index.js
+++
@@ -1,4 +0,0 @@
-import TypeFluid from "../src/typeFluid.js";
-
-const type = new TypeFluid("type", 15);
-type.start();
